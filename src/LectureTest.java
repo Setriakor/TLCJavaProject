83c6daa2dd7e3f0ad5b85fdce3afb6e7e384e6b5
Setriakor <setriakor@gmail.com>
2020-10-09 17:29:41 +0000

    "initial commit"

---
+++ src/LectureTest.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class LectureTest {
+    Lecture TLC= new Lecture();
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        Student s1= new Student();
+        Student s2= new Student();
+        s1.addGrade(5.5);
+        s2.addGrade(4.5);
+        TLC.enter(s1);
+        TLC.enter(s2);
+    }
+
+    @org.junit.jupiter.api.Test
+    void getHighestAverageGrade() {
+        assertEquals(5.5,TLC.getHighestAverageGrade());
+    }
+}
